5f5ea8c7d0e5acde8fdd67c2c0e8603ef035f30f
Marvien Castillo <160254067+MarvienCastillo@users.noreply.github.com>
2026-03-07 01:36:46 +0800

    a

--- CEDISPMP.m
+++ CEDISPMP.m
@@ -1,9 +1,18 @@
-Fs = 455680;
-take_3 = audioread("Take1.wav");
-t_final = (0:length(take_3)-1) / Fs;
-plot(t_final,take_3);
-start_ax = round(0.166406 * Fs);
-end_ax = round(0.18542 * Fs);
+[take_3,Fs] = audioread("Take1.wav"); % take the array data and Fs
+plot(t_final,take_3); % useful for plotting the x coordinate of every vowel
+silence_samples = 1 * Fs; % silence
+pause_vector = zeros(1, silence_samples); % pause vector
+start_ax = round(1.51792 * Fs); % start x axis of 'a' vowel
+end_ax = round(1.7724 * Fs); % end x axis of 'a' vowel
 
-a_vowel = take_3(start_ax:end_ax);
-soundsc(a_vowel);
+start_ox = round(6.5824 * Fs); % start x axis of 'o' vowel
+end_ox = round(6.85265 * Fs); % end x axis of 'o' vowel
+
+start_ix = round(8.69154 * Fs); % start x axis of 'i' vowel
+end_ix = round(8.8129 * Fs); % start x axis of 'i' vowel
+
+a_vowel = take_3(start_ax:end_ax); % sliced array for 'a' vowel
+o_vowel = take_3(start_ox:end_ox); % sliced array for 'o' vowel
+i_vowel = take_3(start_ix:end_ix); % sliced array for 'i' vowel
+all_vowel = [a_vowel; pause_vector'; o_vowel; pause_vector'; i_vowel]; % combined sliced array of vowels and added pause_vector
+soundsc(all_vowel,Fs); % testing sound
